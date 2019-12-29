d0c749be75546f9c5cb54ad88570f3c0e40a578e
Hugh O'Reilly <horeilly1101@gmail.com>
2019-12-29 14:22:48 -0600

    added some more helpful docs

--- rsa/quadratic_reciprocity.py
+++ rsa/quadratic_reciprocity.py
@@ -5,14 +5,17 @@ def compute_jacobi_symbol(x, modulus):
     if x == 0:
         return 0
 
+    # ensure x is a residue mod the modulus
     if not 0 < x < modulus:
         return compute_jacobi_symbol(
             x % modulus, modulus
         )
 
+    # trivial case
     if x == 1:
         return 1
 
+    # base case given by quadratic reciprocity
     if x == 2:
         if modulus % 8 == 1 or modulus % 8 == 7:
             return 1
@@ -20,18 +23,19 @@ def compute_jacobi_symbol(x, modulus):
 
     # divide out the largest even factor
     even_factorization = EvenFactorization.factor(x)
-
     if even_factorization.two_power > 0:
         return (
             pow(compute_jacobi_symbol(2, modulus), even_factorization.two_power)
             * compute_jacobi_symbol(even_factorization.base, modulus)
         )
 
+    # another base case given by quadratic reciprocity
     if x == modulus - 1:
         if modulus % 4 == 1:
             return 1
         return -1
 
+    # recursive case given by quadratic reciprocity
     if x % 4 == 1 or modulus % 4 == 1:
         return compute_jacobi_symbol(modulus, x)
 
